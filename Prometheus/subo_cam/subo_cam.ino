aa475414a60de5a665b1441d3038d42e68723a2b
Kreinicker Gábor <94311591+kreini@users.noreply.github.com>
2023-01-25 10:30:44 +0100

    Add files via upload

---
+++ Prometheus/subo_cam/subo_cam.ino
@@ -0,0 +1,18 @@
+void setup() {
+  pinMode(12, OUTPUT);
+  digitalWrite(12, LOW);
+  delay(3000);
+  digitalWrite(12, HIGH);
+  delay(1000);
+}
+
+void loop() {
+  digitalWrite(12, LOW);
+  delay(200);
+  digitalWrite(12, HIGH);
+  delay(5000);
+  digitalWrite(12, LOW);
+  delay(200);
+  digitalWrite(12, HIGH);
+  delay(200);
+}
